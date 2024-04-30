2bc1ecbc73e23649585f99511af00d9134129752
aMiroslav <163419923+aMiroslav@users.noreply.github.com>
2024-04-30 15:50:16 +0300

    Linijinės regresijos prognozės modelis

---
+++ LR_modelis.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
+import sqlite3
+
+
+# Prisijungiame prie duomenų bazės
+conn = sqlite3.connect('nt_lt.db')
+c = conn.cursor()
+
+    # Pasirenkame visus duomenys iš lentelės
+c.execute("""SELECT * FROM namai_pardavimui""")
+column_names = [description[0] for description in c.description]
+
+    # Sukūriame DF ir uždarome prisijungimą prie DB
+df = pd.DataFrame(c.fetchall(), columns=column_names)
+c.close()
+conn.close()
+
+    # Surušiuojame duomenis į kategorinius ir skaitinius kintamuosius
+categorical_features = ['Pastato tipas:', 'Namo tipas:', 'Šildymas:', 'Vanduo:', 'Įrengimas:',
+                        'Pastato energijos suvartojimo klasė:', 'Miestas:']
+numerical_features = ['Plotas:', 'Kambarių sk.:', 'Sklypo plotas:', 'Statybos Metai:']
+
+    # Nustatome kodavimo kintamuosius
+encoder = LabelEncoder()
+scaler = StandardScaler()
+
+    # Nustatome mūsų duomenys ir tikslą (features, target)
+X = df.drop(['Kaina', 'Rajonas:'], axis=1)
+y = df['Kaina']
+
+    # Padaliname duoemnų rinkinį į mokymo ir testavimo dalis
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+    # Koduojame kategorinius kintamuosius mokymo ir testavimo rinkiniuose
+X_combined = pd.concat([X_train, X_test])
+
+for feature in categorical_features:
+    X_combined[feature] = encoder.fit_transform(X_combined[feature])
+
+X_train_encoded = X_combined[:len(X_train)]
+X_test_encoded = X_combined[len(X_train):]
+
+    # Standartizuojame skaitinius duomenis
+X_train_encoded.loc[:, numerical_features] = scaler.fit_transform(X_train_encoded[numerical_features])
+X_test_encoded.loc[:, numerical_features] = scaler.transform(X_test_encoded[numerical_features])
+
+    # Sukuriame modelį
+model = LinearRegression()
+model.fit(X_train_encoded, y_train)
+
+    # Išvedame modelio tikslumo parametrus
+y_pred = model.predict(X_test_encoded)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f'MSE: {mse}')
+print(f"R2: {r2}")
+
+    # Vizualizuojame modelio spėjimų ir tikrųjų verčių koreliaciją
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, color='blue', alpha=0.5, label='Modelio spėjimai')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2, label='Ideali prognozė')
+plt.title('Santykis tarp tikros ir prognozuotos kainos')
+plt.xlabel('Tikra kaina')
+plt.ylabel('Prognozuota kaina')
+plt.ticklabel_format(style='plain')
+plt.legend()
+plt.show()
