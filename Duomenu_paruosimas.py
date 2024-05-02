37c90f082b4b0745f8a5bd8b552bc3f9c5645343
aMiroslav <163419923+aMiroslav@users.noreply.github.com>
2024-05-02 14:35:11 +0300

    Iš analizės pašalinti duomenys apie NT Palangoje

--- Duomenu_paruosimas.py
+++ Duomenu_paruosimas.py
@@ -4,17 +4,40 @@ import re
 
 
     # Importuojame duomenis nuskaitydami anksčiau paruoštus CSV failus
-df_vilnius = pd.read_csv('Duomenu_isgavimas/namai_vilnius.csv')
-df_kaunas = pd.read_csv('Duomenu_isgavimas/namai_kaunas.csv')
-df_klaipeda = pd.read_csv('Duomenu_isgavimas/namai_klaipeda.csv')
-df_siauliai = pd.read_csv('Duomenu_isgavimas/namai_siauliai.csv')
-df_panevezys = pd.read_csv('Duomenu_isgavimas/namai_panevezys.csv')
-df_alytus = pd.read_csv('Duomenu_isgavimas/namai_alytus.csv')
-df_palanga = pd.read_csv('Duomenu_isgavimas/namai_palanga.csv')
+df_vil = pd.read_csv('Duomenu_isgavimas/namai_vilnius.csv')
+df_kau = pd.read_csv('Duomenu_isgavimas/namai_kaunas.csv')
+df_kl = pd.read_csv('Duomenu_isgavimas/namai_klaipeda.csv')
+df_siau = pd.read_csv('Duomenu_isgavimas/namai_siauliai.csv')
+df_pan = pd.read_csv('Duomenu_isgavimas/namai_panevezys.csv')
+df_al = pd.read_csv('Duomenu_isgavimas/namai_alytus.csv')
 print('Duomenys nuskaityti.')
 
+
+    # Funkcija, skirta pašalinti nereikalingus ženklus iš langelių
+def cleen_cell_to_number(text):
+    numbers = re.sub(r'[^\d.,]', '', text)
+    numbers = numbers.replace(',', '.')
+    return numbers
+
+
+    # Funkcija, skirta pašalinti ribinėms kainų reikšmėms
+def remove_extreme_values(df):
+    df['Kaina'] = df['Kaina'].apply(cleen_cell_to_number)
+    df['Kaina'] = pd.to_numeric(df['Kaina'])
+    df_sorted = df.sort_values(by='Kaina')
+    df_trimmed = df_sorted.iloc[20:-20]
+    return df_trimmed
+
+    # Pritaikome funkciją ir pašaliname ribines reikšmes
+df_vilnius = remove_extreme_values(df_vil)
+df_kaunas = remove_extreme_values(df_kau)
+df_klaipeda = remove_extreme_values(df_kl)
+df_siauliai = remove_extreme_values(df_siau)
+df_panevezys = remove_extreme_values(df_pan)
+df_alytus = remove_extreme_values(df_al)
+
     # Sujungiame visus duomenis į vieną DataFrame
-df = pd.concat([df_vilnius, df_kaunas, df_klaipeda, df_siauliai, df_panevezys, df_alytus, df_palanga],
+df = pd.concat([df_vilnius, df_kaunas, df_klaipeda, df_siauliai, df_panevezys, df_alytus],
                ignore_index=True)
 print('Duomenų rinkiniai sujungti.')
 
@@ -27,33 +50,19 @@ print('Besidubliuojančios reokšmės pašalintos.')
 df_cleaned = df_no_duplicates.drop(['Artimiausias vandens telkinys:', 'Iki vandens telkinio (m):', 'Ypatybės:',
                                     'Papildomos patalpos:', 'Papildoma įranga:', 'Apsauga:', 'Reklama:',
                                     'Reklama/pasiūlymas:', 'Namo numeris:', 'Aukštų sk.:',
-                                    'Unikalus daikto numeris(RC numeris):'], axis=1)
+                                    'Unikalus daikto numeris(RC numeris):', 'Pastato energijos suvartojimo klasė:',
+                                    'Vanduo:', 'Šildymas:', 'Namo tipas:', 'Pastato tipas:'], axis=1)
 print('Pašalinti analizėje nenaudojami stulpeliai.')
 
-    # Kategoriniuose stulpeliuose, kuriuose trūksta verčių, įvedame naują kategoriją apie nepateiktus duomenys
-df_cleaned.fillna({'Pastato energijos suvartojimo klasė:': 'nepateikta'}, inplace=True)
-df_cleaned.fillna({'Vanduo:': 'nepateikta'}, inplace=True)
-df_cleaned.fillna({'Šildymas:': 'nepateikta'}, inplace=True)
-print('Sutvarkytos trūkstamos reikšmės.')
-
-    # Funkcija, skirta pašalinti nereikalingus ženklus iš langelių
-def cleen_cell_to_number(text):
-        numbers = re.sub(r'[^\d.,]', '', text)
-        numbers = numbers.replace(',', '.')
-        return numbers
-
     # Pritaikome funkciją ir konvertuojame duomenys į skaičius pasirinktuose stulpeliuose
-df_cleaned['Kaina'] = df_cleaned['Kaina'].apply(cleen_cell_to_number)
-df_cleaned['Kaina'] = pd.to_numeric(df_cleaned['Kaina'])
 
 df_cleaned['Plotas:'] = df_cleaned['Plotas:'].apply(cleen_cell_to_number)
-df_cleaned['Plotas:'] = pd.to_numeric(df_cleaned['Plotas:'])
 df_cleaned['Plotas:'] = pd.to_numeric(df_cleaned['Plotas:']).round(2)
 
 df_cleaned['Sklypo plotas:'] = df_cleaned['Sklypo plotas:'].apply(cleen_cell_to_number)
 df_cleaned['Sklypo plotas:'] = pd.to_numeric(df_cleaned['Sklypo plotas:']).round(2)
 
-print("Stulpelių 'Kaina', 'Plotas:' ir 'Sklypo plotas:' reikšmės pakeistos į skaitines.")
+print("Stulpelių 'Plotas:' ir 'Sklypo plotas:' reikšmės pakeistos į skaitines.")
 
     # Apskaičiuojame vidutinį kambario plotą, remiantis esamais duomenimis
 df_cleaned['Kambario plotas'] = df_cleaned['Plotas:'] / df_cleaned['Kambarių sk.:']
@@ -82,9 +91,8 @@ print("Sutvarkytas stulpelis 'Metai:', pakesitas į 'Statybos Metai:'.")
 
     # Adreso stulpelį pertvarkome į du atskirus: Miestas ir Rajonas
 df_cleaned['Miestas:'] = df['Adresas'].str.split(',').str[0].str.strip()
-df_cleaned['Rajonas:'] = df['Adresas'].str.split(',').str[1].str.strip()
 df_cleaned.drop(['Adresas'], axis=1, inplace=True)
-print("Sutvarkytas stulpelis 'Adresas', pakeistas į du naujus stulpelius: 'Miestas:', 'Rajonas:'.")
+print("Sutvarkytas stulpelis 'Adresas', pakeistas į stulpelį: 'Miestas:'.")
 
     # Sutvarkome miestų pavadinimus stulpeliuose
 all_cities = df_cleaned['Miestas:'].unique()
@@ -101,17 +109,8 @@ df_cleaned['Miestas:'] = df_cleaned['Miestas:'].replace('Panevėžio r. sav.', '
 df_cleaned['Miestas:'] = df_cleaned['Miestas:'].replace('Šiaulių r. sav.', 'Šiaulių r.')
 df_cleaned['Miestas:'] = df_cleaned['Miestas:'].replace('Alytaus r. sav.', 'Alytaus r.')
 
-    # Sutvarkome kategorinių kintamųjų reikšmes 'Šildymas:' stulpelyje
-df_cleaned['Šildymas:'] = df_cleaned['Šildymas:'].str.split(',').str[0]
-print("Sutvarkytas stulpelis 'Šildymas:'. Paliktos tik pirmu pasirinkimu įrašytos reikšmės.")
 
-
-    # Sutvarkome kategorinių kintamųjų reikšmes 'Vanduo' stulpelyje
-df_cleaned['Vanduo:'] = df_cleaned['Vanduo:'].str.split(',').str[0]
-print("Sutvarkytas stulpelis 'Vanduo:'. Paliktos tik pirmu pasirinkimu įrašytos reikšmės.")
-
-
-    # Sutvarkytų duomenų išsaugojimas duomenų bazėje
+    #Sutvarkytų duomenų išsaugojimas duomenų bazėje
 conn = sqlite3.connect('nt_lt.db')
 c = conn.cursor()
 
